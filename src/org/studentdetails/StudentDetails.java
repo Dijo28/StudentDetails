a6020b724b10cabfd80b3ee584c8c3ab6cc68372
Dijo28 <antodijo148@gmail.com>
2024-10-29 17:24:57 +0530

    code added

---
+++ src/org/studentdetails/StudentDetails.java
@@ -0,0 +1,22 @@
+package org.studentdetails;
+
+public class StudentDetails {
+	public static void main(String[] args) {
+		short studentId = 1234;
+		System.out.println("Student ID is : "+studentId);
+		String studentName = "Sakthi";
+		System.out.println("Student name is : "+studentName);
+		byte mark1 = 90;
+		System.out.println("Tamil mark : "+mark1);
+		byte mark2 = 85;
+		System.out.println("English mark : "+mark2);
+		byte mark3 = 92;
+		System.out.println("Maths mark : "+mark3);
+		byte mark4 = 86;
+		System.out.println("Science mark : "+mark4);
+		byte mark5 = 82;
+		System.out.println("Social mark : "+mark5);
+		
+		
+	}
+}
